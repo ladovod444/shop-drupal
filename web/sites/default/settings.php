1d791be8ba2a7c1a481b1ace3c5a8ae3e438c069
Dmitrii Zadorozhnyi <dmitrii_zadorozhnyi@epam.com>
2024-05-12 11:52:37 +0500

    initial config

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -893,4 +893,4 @@ $databases['default']['default'] = array (
   'namespace' => 'Drupal\\mysql\\Driver\\Database\\mysql',
   'autoload' => 'core/modules/mysql/src/Driver/Database/mysql/',
 );
-$settings['config_sync_directory'] = 'sites/default/files/config_z0myGwxqo-eTdPyfh-Q8X475rQDT1dSxs5ce2fmrNdJLxGJjZT_wZZr1hl8XejkGML1yOzRs_g/sync';
+$settings['config_sync_directory'] = '../config';
